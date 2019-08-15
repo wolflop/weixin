f97515e401fd3da16a9271310276b64c130f3031
wolflop <35335371+wolflop@users.noreply.github.com>
2019-08-15 20:49:44 +0800

    Update main.py

--- main.py
+++ main.py
@@ -4,11 +4,12 @@
 import web
 from handle import Handle
 
+#指定url，并指定handle的类
 urls = (
         '/wx', 'Handle',
 )
 
 if __name__ =='__main__':
-    app = web.application(urls, globals())
-    app.run()
+    app = web.application(urls, globals()) #创建web实例  
+    app.run()        #运行
 
